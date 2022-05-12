acddcd6806c55ae79953cc48eaa1a74e4f0ab309
islo123 <43340489+islo123@users.noreply.github.com>
2022-05-12 17:19:15 +0300

    Update UseFetch.js

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -4,17 +4,15 @@ const url = 'https://api.openbrewerydb.org/breweries';
 
 export default function UseFetch() {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true)
     useEffect(() => {
         const getData = async () => {
         const response = await fetch(url)
         const breweries = await response.json()
-        setLoading(false)
         setData(breweries);
 
         }
         getData()
     }, [url])
 
-    return {data, loading}
+    return {data}
 }
